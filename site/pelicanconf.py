b8946a80b5031225f9ae43ccde2b97f56f53bca1
Goran Mekić <meka@tilda.center>
2016-04-20 15:06:19 +0200

    32C3

--- site/pelicanconf.py
+++ site/pelicanconf.py
@@ -35,7 +35,7 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
-THEME = 'clean-blog'
+THEME = 'theme'
 DELETE_OUTPUT_DIRECTORY = True
 INDEX_SAVE_AS = 'blog/index.html'
 DELETE_OUTPUT_DIRECTORY = True
